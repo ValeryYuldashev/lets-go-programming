af9beaf9b234c33ff23d9d0a83a8ecace44c12b2
Yuldashev Valery <valera4780@gmail.com>
2023-10-30 17:30:23 +0300

    Fix Ex2

--- Ex2/cmd/file.go
+++ Ex2/cmd/file.go
@@ -1,36 +1,17 @@
 package main
 
-import "math"
-
-func getLast(a []int) int {
-	return a[len(a)-1]
-}
-
-func contains(a []int, e int) bool {
-	for _, v := range a {
-		if v == e {
-			return true
-		}
-	}
-	return false
-}
-
 func findKthLargest(nums []int, k int) int {
-	var maxIds []int
-	for i := 0; i < k; i++ {
-		lastMax := math.MinInt
-		var lastMaxId int
-		for i1, v1 := range nums {
-			if contains(maxIds, i1) {
-				continue
-			}
-			if v1 > lastMax {
-				lastMax = v1
-				lastMaxId = i1
-			}
+	lenght := len(nums)
+
+	for i := 1; i < lenght; i++ {
+		j := i - 1
+		v := nums[i]
+		for j >= 0 && nums[j] < v {
+			nums[j+1] = nums[j]
+			j--
 		}
-		maxIds = append(maxIds, lastMaxId)
+		nums[j+1] = v
 	}
 
-	return nums[getLast(maxIds)]
+	return nums[k]
 }
