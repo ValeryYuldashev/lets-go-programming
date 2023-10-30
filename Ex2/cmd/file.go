3de32457ac2a006608cc8c3f0e08aeef7407d08b
Yuldashev Valery <valera4780@gmail.com>
2023-10-30 17:50:47 +0300

    Add quickSort

--- Ex2/cmd/file.go
+++ Ex2/cmd/file.go
@@ -1,17 +1,28 @@
 package main
 
-func findKthLargest(nums []int, k int) int {
-	lenght := len(nums)
-
-	for i := 1; i < lenght; i++ {
-		j := i - 1
-		v := nums[i]
-		for j >= 0 && nums[j] < v {
-			nums[j+1] = nums[j]
-			j--
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] > pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
 		}
-		nums[j+1] = v
 	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
 
+func quickSort(a []int, min int, max int) []int {
+	if min < max {
+		a, pivot := partition(a, min, max)
+		a = quickSort(a, min, pivot-1)
+		a = quickSort(a, pivot+1, max)
+	}
+	return a
+}
+
+func findKthLargest(nums []int, k int) int {
+	quickSort(nums, 0, len(nums)-1)
 	return nums[k-1]
 }
