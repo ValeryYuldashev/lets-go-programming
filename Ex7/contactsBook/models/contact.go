5067bff79b4e143659a13176d57932ca9ba35074
Yuldashev Valery <valera4780@gmail.com>
2023-12-04 19:15:01 +0300

    Ex7 fix

--- Ex7/contactsBook/models/contact.go
+++ Ex7/contactsBook/models/contact.go
@@ -3,6 +3,7 @@ package models
 import (
 	u "contactsBook/utils"
 	"fmt"
+	"unicode"
 
 	"github.com/jinzhu/gorm"
 )
@@ -24,11 +25,23 @@ func (contact *Contact) ValidateContact() (map[string]interface{}, bool) {
 		return u.Message(false, "Phone number cannot be empty!"), false
 	}
 
-	if contact.UserId <= 0 {
-		return u.Message(false, "User not found!"), false
+	for _, v := range contact.Name {
+		if unicode.IsDigit(v) {
+			return u.Message(false, "Invalid name!"), false
+		}
 	}
 
-	return u.Message(true, "success"), true
+	for _, v := range contact.Phone {
+		if !unicode.IsDigit(v) {
+			return u.Message(false, "Invalid phone number!"), false
+		}
+	}
+
+	if len(contact.Phone) != 11 {
+		return u.Message(false, "Phone number cannot be empty!"), false
+	}
+
+	return u.Message(true, "Check is passed!"), true
 }
 
 func (contact *Contact) CreateContact() map[string]interface{} {
@@ -39,7 +52,7 @@ func (contact *Contact) CreateContact() map[string]interface{} {
 
 	GetDB().Create(contact)
 
-	resp := u.Message(true, "success")
+	resp := u.Message(true, "Contact created")
 	resp["contact"] = contact
 	return resp
 }
@@ -84,7 +97,7 @@ func UpdateContatct(id uint, data Contact) map[string]interface{} {
 
 	GetDB().Save(contact)
 
-	resp := u.Message(true, "success")
+	resp := u.Message(true, "Contact updated")
 	resp["contact"] = contact
 	return resp
 }
@@ -99,6 +112,6 @@ func DeleteContact(id uint) map[string]interface{} {
 
 	GetDB().Delete(contact).Where("id = ?", id)
 
-	resp := u.Message(true, "success")
+	resp := u.Message(true, "Contact deleted")
 	return resp
 }
